83a104f5d3a3ec785f488f74082d0636ebff3487
BykadorovR <bykadorov.r@gmail.com>
2019-11-15 23:16:48 +0300

    Project structure

--- include/ECS/General.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include <bitset>
-#include <vector>
-#include <map>
-#include <array>
-#include <memory>
-
-using namespace std;
--- include/Graphic/Loop.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-void on_surface_created();
-void on_surface_changed();
-void on_draw_frame();
-void on_draw_frame2();
-void reshape(int w, int h);
-void update(int value);
